74c8d9cc04119cb67010b97b5b3cb34f0e48921b
Yves Iradukunda <webmineyves22@gmail.com>
2020-09-26 23:07:42 +0200

    first task

---
+++ components/table.php
@@ -0,0 +1,26 @@
+<?php 
+  require_once "global.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Table</title>
+
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+  <?php require_once"meta.php" ?>
+</head>
+<body>
+   <div class="container table-container">
+      <div class="col-md-12">
+      <h1>Sample Table</h1>
+          <table class="table table-responsive">
+
+          </table>
+      </div>
+   </div>
+   
+</body>
+</html>
