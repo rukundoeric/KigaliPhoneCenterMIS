a9376e416acb79856860bf1df393cbb386620e22
wemajosias <51242580+wemajosias@users.noreply.github.com>
2020-09-27 22:30:36 +0200

    login page

---
+++ logOf.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>logof</title>
+		<meta charset="utf-8">
+	        <link rel="stylesheet" href="css/bootstrap.min.css">
+	        <link rel="stylesheet" href="css/font-awesome.min.css">
+	        <link rel="stylesheet" href="css/animate.min.css">
+	        <link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<section>
+		    <div class="container">
+		        <div class="row">
+		            <div class="col-md-4"></div>
+		            <div class="col-md-4">
+			            <div class="profil-image">
+			                 <img src="img/ppp.png"  class="img-circle"> 
+			            <div class="std-name" style="margin-top:20px;">
+                                <p class="text-center">JOSIAS LESSA</p>
+                            </div>
+			            </div>
+		                <div class="logOfForm">
+		                 	<form action="" method="">
+				  				<div class="form-group">
+				  					<input type="password" name="" class="form-control" required placeholder="Your Password ">
+				  				    <input type="submit" class="btn btn-block" value="LOGIN" />
+				  				</div>
+		  					</form>
+		  				</div>
+		  				<samp>FORGOT PASSWORD ? <a href="mailto:josiaslessa@gmail.com">PLEASE CONTACT YOUR ADMINISTATOR</a>
+		  				</samp>
+		            </div>
+		            <div class="col-md-4"></div>
+		        </div>       
+		    </div>
+		</section>
+	    <script type="text/javascript" src="js/jquery.js"></script>
+	    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+	</body>
+</html>
