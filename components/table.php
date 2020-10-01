ff74ba994932a1b66253dd3ae378ab435ea87bbb
Yves Iradukunda <webmineyves22@gmail.com>
2020-10-01 23:00:33 +0200

    table

--- components/table.php
+++ components/table.php
@@ -1,26 +1,40 @@
-<?php 
-  require_once "global.php";
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Table</title>
-
-  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-
-  <?php require_once"meta.php" ?>
-</head>
-<body>
-   <div class="container table-container">
-      <div class="col-md-12">
-      <h1>Sample Table</h1>
-          <table class="table table-responsive">
-
-          </table>
-      </div>
-   </div>
-   
-</body>
-</html>
+<div class="container">
+    <div class="col-md-12 table-container">
+      <div><h4>LATEST ORDERS</h4></div>
+        <table class="table table-hover table-sm   tablecustom">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Product Name</th>
+              <th>Product Type</th>
+              <th>Date</th>
+              <th>Payment Status</th>
+              <th>Shipping Status</th>
+              <th>Order Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td><div><span class="badge badge-id badge-light">1</span></div></td>
+              <td class=""><div><span>Galaxy Note 7nvjndfvndvnmc,xnvcxnvccnvvnvmcnvjk</span></div></td>
+              <td><div><span>Samsung</span></td>
+              <td><div><span>12/12/2020</span></td>
+              <td><div><span class="status-warning badge badge-light">80%</span></div></td>
+              <td><div><span class="badge badge-status badge-warning">Pending</span></div></td>
+              <td><div><span class="badge badge-status badge-warning">Pending</span></div></td>
+            </tr>
+        
+            <tr>
+              <td><div><span class="badge badge-id badge-light">3</span></div></td>
+              <td><div><span>Iphone 8 Plus</span></div></td>
+              <td><div><span>Iphone</span></div></td>
+              <td><div><span>12/10/2020</span></div></td>
+              <td><div><spa><h6 class="status-primary badge badge-light">80%</h6></spa></div></td>
+              <td><div><span class="badge badge-status badge-primary">Arrived</span></div></td>
+              <td><div><span class="badge badge-status badge-success">Done</span></div></td>
+            </tr>
+            
+          </tbody>
+        </table>
+    </div>
+  </div>
--- index.php
+++ index.php
@@ -7,7 +7,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <?php require_once"meta.php" ?>
 </head>
 <body>
+  <?php require_once"components/table.php" ?>
 </body>
 </html>
